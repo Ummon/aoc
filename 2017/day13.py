33378709a3a316fa0ead87d8dc82df9e132419e9
Greg Burri <greg.burri@gmail.com>
2017-12-13 15:01:56 +0100

    It fixes the input "0: <n>"

--- 2017/day13.py
+++ 2017/day13.py
@@ -4,9 +4,9 @@
 with open('day13.input') as f:
     s = [tuple(int(n) for n in l.strip().split(': ')) for l in f.readlines()]
 
-run = lambda s, d: sum((i+d) % (2*(r-1)) == 0 and (i+d) * r for i, r in s)
-r1, r2, c = run(s, 0), 0, 1
+run = lambda s, d, prout: sum((i+d) % (2*(r-1)) == 0 and (prout or i * r) for i, r in s)
+r1, r2, c = run(s, 0, False), 0, 1
 while c != 0:
-    r2, c = r2 + 1, run(s, r2)
+    r2, c = r2 + 1, run(s, r2, True)
 
 print("Solutions: [{}] [{}]".format(r1, r2-1))
